c9d5c64484994ea604a6eacfa36247515db291ff
San Mehat <san@google.com>
2009-07-30 07:55:28 -0700

    mmc: core: Add deferred bus resume policy.
    A card driver can now specify that the underlying bus should *not*
    auto-resume with the rest of the system. This is useful for reducing resume
    latency as well as saving power when the card driver is not using the
    bus. In the future, we'll add support for manual suspend
    
    Change-Id: I077d7dc9311ff12e6e16de631abeac965c8facd9
    Signed-off-by: San Mehat <san@google.com>
    Git-commit: b44e6c88fc57e08562ff6b4fd68ba89cc2aa21bc
    Git-repo: git://git-android.quicinc.com/kernel/msm-3.10
    [asutoshd@codeaurora.org: merge conflicts resolved and
    similar changes were updated in a different file; due to
    changes in kernel version]
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/mmc/core/bus.c
+++ drivers/mmc/core/bus.c
@@ -156,6 +156,8 @@ static int mmc_bus_suspend(struct device *dev)
 			return ret;
 	}
 
+	if (mmc_bus_needs_resume(host))
+		return 0;
 	ret = host->bus_ops->suspend(host);
 	return ret;
 }
@@ -167,11 +169,17 @@ static int mmc_bus_resume(struct device *dev)
 	struct mmc_host *host = card->host;
 	int ret;
 
+	if (host->bus_resume_flags & MMC_BUSRESUME_MANUAL_RESUME) {
+		host->bus_resume_flags |= MMC_BUSRESUME_NEEDS_RESUME;
+		goto skip_full_resume;
+	}
+
 	ret = host->bus_ops->resume(host);
 	if (ret)
 		pr_warn("%s: error %d during resume (card was removed?)\n",
 			mmc_hostname(host), ret);
 
+skip_full_resume:
 	if (dev->driver && drv->resume)
 		ret = drv->resume(card);
 
@@ -185,6 +193,9 @@ static int mmc_runtime_suspend(struct device *dev)
 	struct mmc_card *card = mmc_dev_to_card(dev);
 	struct mmc_host *host = card->host;
 
+	if (mmc_bus_needs_resume(host))
+		return 0;
+
 	return host->bus_ops->runtime_suspend(host);
 }
 
@@ -192,6 +203,9 @@ static int mmc_runtime_resume(struct device *dev)
 {
 	struct mmc_card *card = mmc_dev_to_card(dev);
 	struct mmc_host *host = card->host;
+
+	if (mmc_bus_needs_resume(host))
+		host->bus_resume_flags &= ~MMC_BUSRESUME_NEEDS_RESUME;
 
 	return host->bus_ops->runtime_resume(host);
 }
