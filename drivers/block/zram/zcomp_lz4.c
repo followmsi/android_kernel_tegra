5677a9fda6d7b440e6498af33f30470fd4035eca
Sonny Rao <sonnyrao@chromium.org>
2016-12-02 18:51:52 -0800

    Revert "CHROMIUM: zram: lz4: fallback to vmalloc allocation"
    This reverts commit 90407b381f3fea4fc31382b06586eba803d8a48a.
    
    It is superceded by the upstream commit
      d913897 zram: try vmalloc() after kmalloc()
    
    BUG=chromium:670864
    TEST=build elm
    
    Change-Id: If4b345034156512833d503a64789939c00f196ca
    Reviewed-on: https://chromium-review.googlesource.com/416644
    Commit-Ready: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>

--- drivers/block/zram/zcomp_lz4.c
+++ drivers/block/zram/zcomp_lz4.c
@@ -8,30 +8,19 @@
  */
 
 #include <linux/kernel.h>
-#include <linux/mm.h>
 #include <linux/slab.h>
-#include <linux/vmalloc.h>
 #include <linux/lz4.h>
 
 #include "zcomp_lz4.h"
 
 static void *zcomp_lz4_create(void)
 {
-	void *buf;
-	size_t size = LZ4_MEM_COMPRESS;
-
-	/* fallback to vmalloc allocation */
-	buf = kzalloc(size, GFP_KERNEL | __GFP_NORETRY | __GFP_NOWARN);
-	if (!buf && size > PAGE_SIZE) {
-		buf = vmalloc(size);
-		memset(buf, 0, size);
-	}
-	return buf;
+	return kzalloc(LZ4_MEM_COMPRESS, GFP_KERNEL);
 }
 
 static void zcomp_lz4_destroy(void *private)
 {
-	kvfree(private);
+	kfree(private);
 }
 
 static int zcomp_lz4_compress(const unsigned char *src, unsigned char *dst,
