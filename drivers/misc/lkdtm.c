c661f3c36808ff3d807ac1ef1ea7048e6f90a400
Michael Davidson <md@google.com>
2017-04-14 14:15:09 -0700

    BACKPORT: misc: lkdtm: Add volatile to intentional NULL pointer reference
    Add a volatile qualifier where a NULL pointer is deliberately
    dereferenced to trigger a panic.
    
    Without the volatile qualifier clang will issue the following warning:
    "indirection of non-volatile null pointer will be deleted,
    not trap [-Wnull-dereference]" and replace the pointer reference
    with a __builtin_trap() (which generates a ud2 instruction on x86_64).
    
    Change-Id: If25c00dd39a67078d5c183c5b1190cb6c2c30357
    Signed-off-by: Michael Davidson <md@google.com>
    Signed-off-by: Matthias Kaehlcke <mka@chromium.org>
    Acked-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://git.kernel.org/linus/9e18308a5dcc2250a271e598dfe0d917b5522475
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/misc/lkdtm.c
+++ drivers/misc/lkdtm.c
@@ -376,7 +376,6 @@ static void lkdtm_do_action(enum ctype which)
 		WARN_ON(1);
 		break;
 	case CT_EXCEPTION:
-		/* volatile to keep clang happy */
 		*((volatile int *) 0) = 0;
 		break;
 	case CT_LOOP:
