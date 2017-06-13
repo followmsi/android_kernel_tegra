64d722c807929aac9bde353b90985b5d147d00ec
Sonny Rao <sonnyrao@chromium.org>
2017-06-12 22:34:36 -0700

    CHROMIUM: low-mem: handle multiple levels and provide edge notifications
    The changes low-mem to add multiple levels for notification and
    notification based on polling sysfs files rather than
    /dev/chromeos-low-mem.
    
    BUG=chromium:736538
    TEST=run telemetry tab_switching test on caroline with 5 pagesets and
      observe discards ocurring
    
    Change-Id: I7c4b2c5cbc9e0c29d7dc953a47f4eda41c0cb454
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/536336
    Reviewed-by: Luigi Semenzato <semenzato@chromium.org>

--- include/linux/low-mem-notify.h
+++ include/linux/low-mem-notify.h
@@ -5,14 +5,24 @@
 #include <linux/stddef.h>
 #include <linux/swap.h>
 
-extern unsigned low_mem_margin_percent;
-extern unsigned long low_mem_minfree;
+/* We support up to this many different thresholds. */
+#define LOW_MEM_THRESHOLD_MAX 5
+
+extern unsigned long low_mem_thresholds[];
+extern unsigned int low_mem_threshold_count;
+extern unsigned int low_mem_threshold_last;
 void low_mem_notify(void);
 extern const struct file_operations low_mem_notify_fops;
 extern bool low_mem_margin_enabled;
 extern unsigned long low_mem_lowest_seen_anon_mem;
 extern const unsigned long low_mem_anon_mem_delta;
 extern unsigned int low_mem_ram_vs_swap_weight;
+
+#ifdef CONFIG_SYSFS
+extern void low_mem_threshold_notify(void);
+#else
+static inline void low_mem_threshold_notify(void) { }
+#endif
 
 /*
  * Compute available memory used by files that can be reclaimed quickly.
@@ -61,10 +71,8 @@ static inline unsigned long get_available_mem_adj(int lru_base)
 	return available_mem + swap_adj;
 }
 
-/*
- * Returns TRUE if we are in a low memory state.
- */
-static inline bool _is_low_mem_situation(void)
+#ifdef CONFIG_LOW_MEM_NOTIFY
+static inline bool low_mem_check(void)
 {
 	const int lru_base = NR_LRU_BASE - LRU_BASE;
 	static bool was_low_mem;	/* = false, as per style guide */
@@ -72,7 +80,16 @@ static inline bool _is_low_mem_situation(void)
 	 * space is low.
 	 */
 	unsigned long available_mem = get_available_mem_adj(lru_base);
-	bool is_low_mem = available_mem < low_mem_minfree;
+	/*
+	 * For backwards compatibility with the older margin interface, we will trigger
+	 * the /dev/chromeos-low_mem device when we are below the lowest threshold
+	 */
+	bool is_low_mem = available_mem < low_mem_thresholds[0];
+	unsigned int threshold_lowest = UINT_MAX;
+	int i;
+
+	if (!low_mem_margin_enabled)
+		return false;
 
 	if (unlikely(is_low_mem && !was_low_mem)) {
 		unsigned long anon_mem =
@@ -92,14 +109,27 @@ static inline bool _is_low_mem_situation(void)
 				low_mem_anon_mem_delta;
 		}
 	}
+
 	was_low_mem = is_low_mem;
+	if (is_low_mem)
+		low_mem_notify();
+
+	for (i = 0; i < low_mem_threshold_count; i++)
+		if (available_mem < low_mem_thresholds[i]) {
+			threshold_lowest = i;
+			break;
+		}
+
+	/* we crossed one or more thresholds */
+	if (unlikely(threshold_lowest < low_mem_threshold_last))
+		low_mem_threshold_notify();
+
+	low_mem_threshold_last = threshold_lowest;
 
 	return is_low_mem;
 }
-
-static inline bool is_low_mem_situation(void)
-{
-	return low_mem_margin_enabled ? _is_low_mem_situation() : false;
-}
+#else
+static inline bool low_mem_check(void) { return false;}
+#endif
 
 #endif
