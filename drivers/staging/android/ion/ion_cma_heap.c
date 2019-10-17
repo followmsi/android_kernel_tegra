081ac7adb9e9266262c399efdfd4dfae8441e9e2
Vijayanand Jitta <vjitta@codeaurora.org>
2019-10-17 13:47:24 -0400

    ion: Fix typecasting of msm_ion_heap_types
    msm_ion_heap_types type variable is assigned to a ion_heap_type
    variable, typecast this properly to ion_heap_type before assigning
    also fix is_cp_flag_present function by using bitwise operators
    instead of logical operators and consider ION_FLAG_SECURE as well
    while allocating through system secure heap.
    
    Change-Id: I7f65c5d5d92e3007a1f803e7438ccbfb672907e6
    Signed-off-by: Vijayanand Jitta <vjitta@codeaurora.org>
    Link: https://source.codeaurora.org/quic/la/kernel/msm-4.9/commit/?id=ad7d1e333f74f1a71f3ed51882221e61ab67dc60
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/staging/android/ion/ion_cma_heap.c
+++ drivers/staging/android/ion/ion_cma_heap.c
@@ -201,7 +201,7 @@ struct ion_heap *ion_cma_heap_create(struct ion_platform_heap *data)
 	/* get device from private heaps data, later it will be
 	 * used to make the link with reserved CMA memory */
 	cma_heap->dev = data->priv;
-	cma_heap->heap.type = ION_HEAP_TYPE_DMA;
+	cma_heap->heap.type = (enum ion_heap_type)ION_HEAP_TYPE_DMA;
 	return &cma_heap->heap;
 }
 
